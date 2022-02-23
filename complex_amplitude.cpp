5e5dda64c1b158ea7153555327ab6ce32652a28c
Laufinsconsca <walle.log@yandex.ru>
2022-02-24 02:20:25 +0400

    Amend

--- complex_amplitude.cpp
+++ complex_amplitude.cpp
@@ -112,56 +112,6 @@ complex_amplitude::complex_amplitude(const class gauss_beam& gauss_beam, const c
     }
 }
 
-///**
-// * Constructor
-// *
-// * @param	gauss_beam  	The gauss beam
-// * @param	spiral          The spiral
-// * @param	size            The size
-// **************************************************************************************************/
-
-//complex_amplitude::complex_amplitude(const class gauss_beam& gauss_beam, const class spiral& spiral, const QSize& size)
-//    : complex_amplitude(gauss_beam, spiral, size, hole{0, 0, 0, hole_type::none}) {}
-
-///**
-// * Constructor
-// *
-// * @param	gauss_beam  	The gauss beam
-// * @param	spiral          The spiral
-// * @param	size            The size
-// **************************************************************************************************/
-
-//complex_amplitude::complex_amplitude(const class gauss_beam& gauss_beam, const class spiral& spiral, const QSize& size, const hole& hole) {
-//    this->size = size;
-//    std::vector<std::vector<double>> gauss_beam_vector_to_fill, spiral_vector_to_fill;
-//    std::vector<std::vector<double>> ref_beam, spiral_vector;
-//    if (hole.type == hole_type::amplitude_hole || hole.type == hole_type::amplitude_phase_hole) {
-//        ref_beam = gauss_beam::gauss_beam_to_vector(gauss_beam, gauss_beam_vector_to_fill, size, hole);
-//    } else {
-//        ref_beam = gauss_beam::gauss_beam_to_vector(gauss_beam, gauss_beam_vector_to_fill, size);
-//    }
-//    spiral_vector = spiral::spiral_to_vector(spiral, spiral_vector_to_fill, size);
-//    double hx, hy;
-//    this->pixels.reserve(size.height());
-//    hx = 2. / size.width();
-//    hy = 2. / size.height();
-//    double x, y;
-//    for (int i = 0; i < size.height(); i++) {
-//        y = i * hy - 1;
-//        std::vector<std::complex<double>> row;
-//        row.reserve(size.width());
-//        for (int j = 0; j < size.width(); j++) {
-//            x = j * hx - 1;
-//            if (pow(x, 2) + pow(y, 2) < 1) {
-//                row.emplace_back(std::polar(ref_beam.at(i).at(j), spiral_vector.at(i).at(j)));
-//            } else {
-//                row.emplace_back(0);
-//            }
-//        }
-//        this->pixels.emplace_back(row);
-//    }
-//}
-
 /**
  * Constructor
  *
@@ -262,55 +212,6 @@ complex_amplitude::complex_amplitude(QImage &amplitude, const class vortex& vort
         pixels.emplace_back(row);
     }
 }
-
-///**
-// * Constructor
-// *
-// * @exceptions	runtime_error	Raised when the amplitude image size is inconsistent with the phase image size
-// *
-// * @param	amplitude	The QImage amplitude
-// * @param	spiral      The spiral
-// **************************************************************************************************/
-
-//complex_amplitude::complex_amplitude(QImage& amplitude, const class spiral& spiral)
-//    : complex_amplitude(amplitude, spiral, hole{0, 0, 0, hole_type::none}) {}
-
-///**
-// * Constructor
-// *
-// * @exceptions	runtime_error	Raised when the amplitude image size is inconsistent with the phase image size
-// *
-// * @param	amplitude	The QImage amplitude
-// * @param	spiral      The spiral
-// * @param   hole        The hole
-// **************************************************************************************************/
-
-//complex_amplitude::complex_amplitude(QImage &amplitude, const class spiral& spiral, const hole &hole) {
-//    amplitude = amplitude.convertToFormat(QImage::Format_Grayscale8);
-//    size = amplitude.size();
-//    std::vector<std::vector<double>> spiral_vector_to_fill, spiral_vector;
-//    if (hole.type == hole_type::amplitude_hole || hole.type == hole_type::amplitude_phase_hole) {
-//        hole::create_hole(amplitude, hole, 1);
-//    }
-//    spiral_vector = spiral::spiral_to_vector(spiral, spiral_vector_to_fill, size);
-//    pixels.reserve(size.height());
-//    double x, y;
-//    for (int i = 0; i < size.height(); i++) {
-//        x = i - size.height()/2;
-//        std::vector<std::complex<double>> row;
-//        row.reserve(size.width());
-//        unsigned char* amplitude_line = amplitude.scanLine(i);
-//        for (int j = 0; j < size.width(); j++) {
-//            y = j - size.width()/2;
-//            if (sqrt(x * x + y * y) < size.width() / 2) {
-//                row.emplace_back(std::polar(static_cast<double>(amplitude_line[j]), spiral_vector.at(i).at(j)));
-//            } else {
-//                row.emplace_back(std::polar(0., 0.));
-//            }
-//        }
-//        pixels.emplace_back(row);
-//    }
-//}
 
 /**
  * Constructor
